616c83d19be9782eb35fd97da6986e84724f870f
Sukhu, Tarun (ts932q) <ts932q@us.att.com>
2017-09-05 16:59:23 +0530

    Initial checkin

---
+++ cqrs-app/src/main/java/com/onefoundation/cqrsdemo/CqrsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.onefoundation.cqrsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqrsDemoApplication.class, args);
+	}
+}
