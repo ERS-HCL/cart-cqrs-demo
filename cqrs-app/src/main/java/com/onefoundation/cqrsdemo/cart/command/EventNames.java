610d08410bf0fd9ece418f7dcf0d74a0988fcfde
Kamran Ali Khan <khankamranali@gmail.com>
2017-11-10 08:26:20 +0530

    added enums for event and DocTypes

---
+++ cqrs-app/src/main/java/com/onefoundation/cqrsdemo/cart/command/EventNames.java
@@ -0,0 +1,6 @@
+package com.onefoundation.cqrsdemo.cart.command;
+
+public enum EventNames {
+	ItemAddedEvent,
+	ItemUpdatedEvent
+}
---
+++ cqrs-app/src/main/java/com/onefoundation/cqrsdemo/cart/store/DocTypes.java
@@ -0,0 +1,6 @@
+package com.onefoundation.cqrsdemo.cart.store;
+
+public enum DocTypes {
+	CartEvent,
+	Cart
+}
